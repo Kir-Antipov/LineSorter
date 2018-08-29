db6033d291f18de0690a9fd24c9ee09ea7332e41
Kir-Antipov <kp_antipov@mail.ru>
2018-08-29 20:29:18 +0300

    LineSorter 2.5
    - Templates are created for convenient addition of new commands
    - Random line mixing command was added

---
+++ LineSorter/Commands/CommandRandomSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using LineSorter.Helpers;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.Shell;
+
+namespace LineSorter.Commands
+{
+    internal sealed class CommandRandomSort : BaseCommand<CommandRandomSort>
+    {
+        #region Var
+        private static Random Rand { get; } = new Random();
+        #endregion
+
+        #region Init
+        static CommandRandomSort()
+        {
+            CommandID = 0x0104;
+            CommandSet = new Guid("e9f69e2b-6313-4c2b-9765-1ddd6439d519");
+        }
+        #endregion
+
+        #region Functions
+        protected override void Execute(OleMenuCommand Button)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            Shuffle(TextSelection.GetSelection(Package, out bool newLine)).ReplaceSelection(newLine);
+        }
+        private static IEnumerable<T> Shuffle<T>(IEnumerable<T> Source)
+        {
+            T[] data = Source.ToArray();
+            for (int i = data.Length - 1; i > -1; --i)
+            {
+                int newIndex = Rand.Next(i + 1);
+                yield return data[newIndex];
+                data[newIndex] = data[i];
+            }
+        }
+        #endregion
+    }
+}
