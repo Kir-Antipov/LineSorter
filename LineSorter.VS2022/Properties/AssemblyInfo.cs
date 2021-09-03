eb56ad608acd078b42efbe0d796fa7210fd4c838
Jan Ivar Z. Carlsen <jizc@outlook.com>
2021-09-03 13:04:32 +0200

    Split extension into PreVS2022 and VS2022 projects

---
+++ LineSorter.VS2022/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+[assembly: AssemblyVersion("5.0.1")]
+[assembly: AssemblyFileVersion("5.0.1")]
+[assembly: NeutralResourcesLanguage("en", UltimateResourceFallbackLocation.Satellite)]
+
+[assembly: ProvideCodeBase]
+[assembly: AssemblyTitle("LineSorter")]
+[assembly: AssemblyProduct("LineSorter")]
+[assembly: AssemblyCompany("Kir_Antipov")]
+[assembly: AssemblyTrademark("LineSorter")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCopyright("Kir_Antipov")]
+[assembly: AssemblyDescription("LineSorter extension")]
+
+[assembly: ComVisible(true)]
